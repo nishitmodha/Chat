6c79533547eee1cc7414c5f53ff3b666dbbf6029
nishitmodha <nishitmodha30@gmail.com>
2021-02-26 15:15:08 +0530

    Added Validations
    Message length restricted and name of user must be unique.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
 	belongs_to :user
 	validates :content, presence: true
+	validates :content, :length => { :maximum => 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
 	has_many :messages
+	validates :name, presence:true, uniqueness: {case_sensitive: false}
 end
