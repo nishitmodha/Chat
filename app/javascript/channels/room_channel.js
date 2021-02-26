b412938076ff5cfe76d1356d3f6278e203d75eb5
bhavik2936 <bhavik2936@gmail.com>
2021-02-26 12:04:54 +0530

    same page message listing available

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -10,6 +10,8 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
+    $('#chat-list').append("<tr><td>" + data.content + "</td><td>" + data.user_name + "</td></tr>");
+    $('#content').val('');
     // Called when there's incoming data on the websocket for this channel
   }
 });
